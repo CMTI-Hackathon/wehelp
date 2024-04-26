f7d93ae3b6c198057da5b2953aef84a974b4abef
Slavchu <tothelove1@gmail.com>
2024-04-26 17:28:26 +0300

    Splitter auth implementation

--- splitter/main.go
+++ splitter/main.go
@@ -1,29 +1,54 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"net/http/httputil"
+	pb "wehelp_goservice/grpc"
+
+	"github.com/golang/protobuf/jsonpb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 type LoginData struct {
 	Login, Password string
 }
 
-func http_function(w http.ResponseWriter, r *http.Request) {
+func registerUser(w http.ResponseWriter, r *http.Request) {
 	res, err := httputil.DumpRequest(r, true)
-
+	if r.Method != "POST" {
+		w.Write([]byte("{\"succes\" : false}"))
+		return
+	}
 	if err == nil {
 		println(string(res))
 	}
-	if r.Method == "GET" {
-		return
-	} else if r.Method == "POST" {
+
+	conn, err := grpc.Dial("auth-service:4011", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		println("error:", err)
+	}
+	client := pb.NewSplitterAuthClient(conn)
+	var request pb.RegisterRequest
+	jsonpb.Unmarshal(r.Body, &request)
+	response, err := client.Register(context.Background(), &request)
+	if err != nil {
+		w.Write([]byte("{\"succes\" : false}"))
 		return
 	}
+	answer, err := protojson.Marshal(response)
+	if err != nil {
+		w.Write([]byte("{\"succes\" : false}"))
+		return
+	}
+	w.Write(answer)
+
 }
 func main() {
-	http.HandleFunc("/login", http_function)
+	http.HandleFunc("/registerUser", registerUser)
 	log.Fatal(http.ListenAndServe(":3035", nil))
 
 }
